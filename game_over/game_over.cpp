706b62e33b25d13c810c747431e1b1a5c4ebb5e7
Zhigang Wang <zhigangw@hotmail.com>
2019-03-09 17:36:06 +0800

    remove precompile files

--- game_over/game_over.cpp
+++ game_over/game_over.cpp
@@ -1,7 +1,6 @@
 // game_over.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
 #include <iostream>
 using namespace std;
 void miner_puzzle();
